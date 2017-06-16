84fd11d7c752dccb710edd6602cf9a434507109d
wblachowski <wojciech@blachowski.pl>
2017-06-16 13:16:40 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,69 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <stdlib.h>
+#include <stdio.h>
+#include "constants.h"
+#include "allmodels.h"
+
+
+//Procedura obsługi błędów
+void error_callback(int error, const char* description) {
+	fputs(description, stderr);
+}
+
+//Procedura inicjująca
+void initOpenGLProgram() {
+	//************Tutaj umieszczaj kod, który należy wykonać raz, na początku programu************
+
+}
+
+//Procedura rysująca zawartość sceny
+void drawScene(GLFWwindow* window) {
+	//************Tutaj umieszczaj kod rysujący obraz******************l
+	
+}
+
+int main(void)
+{
+	GLFWwindow* window; //Wskaźnik na obiekt reprezentujący okno
+
+	glfwSetErrorCallback(error_callback);//Zarejestruj procedurę obsługi błędów
+
+	if (!glfwInit()) { //Zainicjuj bibliotekę GLFW
+		fprintf(stderr, "Nie można zainicjować GLFW.\n");
+		exit(EXIT_FAILURE); 
+	}
+
+	window = glfwCreateWindow(500, 500, "OpenGL", NULL, NULL);  //Utwórz okno 500x500 o tytule "OpenGL" i kontekst OpenGL. 
+
+	if (!window) //Jeżeli okna nie udało się utworzyć, to zamknij program
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window); //Od tego momentu kontekst okna staje się aktywny i polecenia OpenGL będą dotyczyć właśnie jego.
+	glfwSwapInterval(1); //Czekaj na 1 powrót plamki przed pokazaniem ukrytego bufora
+
+	GLenum err;
+	if ((err=glewInit()) != GLEW_OK) { //Zainicjuj bibliotekę GLEW		
+		fprintf(stderr, "Nie można zainicjować GLEW: %s\n", glewGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+
+	initOpenGLProgram(); //Operacje inicjujące
+
+	//Główna pętla
+	while (!glfwWindowShouldClose(window)) //Tak długo jak okno nie powinno zostać zamknięte
+	{		
+		drawScene(window); //Wykonaj procedurę rysującą
+		glfwPollEvents(); //Wykonaj procedury callback w zalezności od zdarzeń jakie zaszły.
+	}
+
+	glfwDestroyWindow(window); //Usuń kontekst OpenGL i okno
+	glfwTerminate(); //Zwolnij zasoby zajęte przez GLFW
+	exit(EXIT_SUCCESS);
+}
